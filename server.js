1235898628c3d61b9ff617a225ef7fb4c432e254
UnicornDad8 <unicorndad8@gmail.com>
2023-09-25 10:51:47 -0400

    Added chat model, messages model also more routes (create new chat route and get all chats route)

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const usersRoute = require("./routes/usersRoute");
+const chatsRoute = require("./routes/chatsRoute");
 app.use(express.json());
 
 app.use("/api/users", usersRoute);
+app.use("/api/chats", chatsRoute);
 
 app.listen(port, () => console.log(`server running on port ${port}`));
