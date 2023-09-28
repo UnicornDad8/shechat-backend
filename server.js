257c363962efee2d303863d224869531421b4599
UnicornDad8 <unicorndad8@gmail.com>
2023-09-28 11:08:35 -0400

    Added messages route with 2 endpoints

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ const port = process.env.PORT || 5000;
 
 const usersRoute = require("./routes/usersRoute");
 const chatsRoute = require("./routes/chatsRoute");
+const messagesRoute = require("./routes/messagesRoute");
 app.use(express.json());
 
 app.use("/api/users", usersRoute);
 app.use("/api/chats", chatsRoute);
+app.use("/api/messages", messagesRoute);
 
 app.listen(port, () => console.log(`server running on port ${port}`));
